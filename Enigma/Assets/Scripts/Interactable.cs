a2044c9481c501994b56b8c2f648ddd37675b98c
hgc498 <40686263+hgc498@users.noreply.github.com>
2021-03-23 12:57:00 +0000

    Inventory Update

--- Enigma/Assets/Scripts/Interactable.cs
+++ Enigma/Assets/Scripts/Interactable.cs
@@ -6,11 +6,53 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
     public float radius = 1f;
+    public Transform interactableTramsform;
 
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interacting with " + transform.name);
+    }
+
+    void Update()
+    {
+        // if we are currnelty being focused
+        //and we havnt already interacted with the object 
+        if(isFocus && !hasInteracted)
+        {
+            //if we are close enough
+            float distance = Vector3.Distance(player.position, interactableTramsform.position);
+            if (distance <= radius)
+            {
+                //interact with the object 
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+    //called whne the objevt starts being focused 
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+    //called whne the object is no longer focused 
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+    //draws a radius in the editor 
     void OnDrawGizmosSelected ()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, radius);
+        Gizmos.DrawWireSphere(interactableTramsform.position, radius);
     }
 
 }
