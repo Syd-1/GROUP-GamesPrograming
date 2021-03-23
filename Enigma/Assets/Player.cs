21292fda20178d774aa72f2124ccd7f08782146d
hgc498 <40686263+hgc498@users.noreply.github.com>
2021-03-23 19:22:37 +0000

    added health bar stuff back

---
+++ Enigma/Assets/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	public int maxHealth = 90;
+	public int currentHealth;
+
+	public HealthBar healthBar;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		currentHealth = maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//needs to change to when hit bby enemy 
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			TakeDamage(30);
+		}
+	}
+
+	void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+
+		healthBar.SetHealth(currentHealth);
+	}
+}
